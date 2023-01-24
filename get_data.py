34ec9ef98b3d8f00e74b69b47d5917317abcb8f3
Erdem Ata <ataerdem@gmail.com>
2023-01-24 23:44:36 +0300

    init

---
+++ get_data.py
@@ -0,0 +1,60 @@
+from pyyoutube import Api
+import requests
+import re
+
+CSV_NAME = "channels.csv"
+DELIMER = ";"
+
+def get_channel_names():
+    """
+    This function gets channel names from channels.csv file.
+    The channel names are at the first column of the csv file.
+    """
+    with open(CSV_NAME, 'r') as f:
+        lines = f.readlines()[1:]
+        channel_names = [line.split(DELIMER)[0].strip() for line in lines]
+    return channel_names
+
+def get_channelid(channel_name):
+    """
+    This function gets the page and uses regex to get the channelId
+    itemprop="channelId" content="channelIdgoeshere"
+    """
+    url = "https://www.youtube.com/c/" + channel_name
+    r = requests.get(url).text
+    channel_id = re.findall(r'itemprop="channelId" content="(.+?)"', r)[0]
+    return channel_id
+
+def get_view_count(ids, n=10):
+    """
+    This function gets the channel object and uses the api to get the average view count of the latest n videos
+    """
+    vid_ids = [[item.to_dict()["contentDetails"]["upload"]["videoId"] for item in api.get_activities_by_channel(channel_id=id_, count=n).items] for id_ in ids]
+    view_counts = [int(sum([get_view(vid_id) for vid_id in vid_id])/n) for vid_id in vid_ids]
+    return view_counts
+
+def get_view(video_id):
+    """
+    This function gets the view count of a video
+    """
+    return int(api.get_video_by_id(video_id=video_id).items[0].statistics.viewCount)
+
+def update_csv(channel_names, view_counts, sub_counts):
+    """
+    This function updates the csv file with the new view counts and subscriber counts
+    """
+    lines = ["Name;View Count;Subscriber Count"]
+    for cn, vc, sc in zip(channel_names, view_counts, sub_counts):
+        lines.append(DELIMER.join([cn, str(vc), str(sc)]))
+    
+    with open(CSV_NAME, 'w') as f:
+        f.write("\n".join(lines))
+
+if __name__ == "__main__":
+    api = Api(api_key="AIzaSyCuvRpAelWkJa0yfd3RjnxYv_3wiAs1dRo")
+    channel_names = get_channel_names()
+    ids = [get_channelid(channel_name) for channel_name in channel_names]
+    channels = api.get_channel_info(channel_id=",".join(ids))
+    sub_counts = [channel.statistics.subscriberCount for channel in channels.items]
+    view_counts = get_view_count(ids)
+    update_csv(channel_names, view_counts, sub_counts)
